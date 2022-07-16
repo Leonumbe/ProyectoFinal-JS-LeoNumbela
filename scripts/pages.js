efef0a9a18281ea4044b004d0d858c0cd7231192
leoNumbela <leonumbela@gmail.com>
2022-07-16 18:58:09 -0300

    Se realizan ajustes previos entrega final del proyecto, se elimina lineas de comentarios

---
+++ scripts/pages.js
@@ -0,0 +1,281 @@
+//declaracion variables golabales
+let reserva;
+let deleteCard;
+let userBooked;
+let buttonSubmit;
+//dates
+let checkIn = document.getElementById("check-in");
+let checkOut = document.getElementById("check-out");
+let checkInDate;
+let checkOutDate;
+
+//Nuestra reserva la guardamos en el localStorage
+//se reemplaza por TERNARIO
+reserva = (JSON.parse(localStorage.getItem('reserva'))) ? 
+    reserva = JSON.parse(localStorage.getItem('reserva')):
+    localStorage.setItem('reserva', JSON.stringify([]))
+    reserva = JSON.parse(localStorage.getItem('reserva'));
+
+
+//--------------boton borrar--------------------------///
+            
+document.addEventListener("click", (e) => {
+    if(e.target && e.target.matches("button.deleteBtn")){
+    const item = reserva.find(prod => prod.id === e.target.id)
+    //obtengo indice del item   
+    const indice = reserva.indexOf(item)
+    //elimino carrito del array
+    reserva.splice(indice, 1)
+    window.location.reload();
+    }
+    localStorage.setItem('reserva', JSON.stringify(reserva))
+})
+
+//---------------page Booked---------------------//
+//Funcion suma n booked
+//reduce ciclo de producto mas acum. Monto total mas fee 0.1
+const totalReserved = () => {
+    return reserva.reduce((acumulador, prod) => acumulador + (prod.price * prod.cantidad, 0 ))
+}
+
+const body = document.getElementById('bookedInProgress');
+//desplegamos reserva en el Dom
+if(reserva.length == 0){
+    const noBookedSelection = 
+        `
+        <h1 class="title subTAdapt">You are not selected any room</h1>
+        <a class="" href="../index.html">
+            <button class="btn">Back</button>
+        <a/>
+        `
+    body.innerHTML += noBookedSelection; 
+   
+    refreshStorage ();  
+    }else{
+    const summary = `
+        <h1 id="bookedSelection"class="subT  h1adapted">Booked Selection</h1>
+        <div id='tBody'></div>
+        <a id="filter" class="btn" href="../index.html">Back</a>
+        <a href="#Form" class="btn">End Booked</a>
+        `
+    body.innerHTML += summary
+
+    const tbody = document.getElementById('tBody')
+    for (let i = 0; i < reserva.length; i++) {
+        const element = reserva[i];
+        const { id, category, name, price, img, cantidad } = element;
+        const reservaFinal =
+         `
+        <div id=${id} >
+            <img class="imgBooked" src="${img}" alt="Imagen de la room"> 
+            <div class='boxBooked'>
+                <div class="head">Name: ${name}</div>
+                <div class="subT">Category: ${category}</div>
+                <div>Quantity booked: ${cantidad}</div>
+                <div class="price">Price per night: $${price.toLocaleString()}- Euros</div>
+                <div>Total Amount: $ ${(cantidad * price * localStorage.getItem("dateTotal")).toLocaleString()}- Euros</div>
+            </div>
+            <button id='${id}' class='btn deleteBtn'>Delete</button>
+        </div>
+        ` 
+        tbody.innerHTML += reservaFinal
+    }
+}
+
+
+//---------------Form---------------------//
+
+//Nuestro formulario es guardado en el localStorage
+//Operador ternario
+userBooked =  (JSON.parse(localStorage.getItem('userBooked'))) ? 
+    userBooked = JSON.parse(localStorage.getItem('userBooked'))
+    : localStorage.setItem('userBooked', JSON.stringify({}))
+    userBooked = JSON.parse(localStorage.getItem('userBooked'));
+
+//Completar formulario
+function endBooked() {
+    class user {
+        constructor(name,surname,email,phone,text){
+            this.name = name,
+            this.surname = surname,
+            this.email = email,
+            this.phone = phone,
+            this.text = text
+        }
+    }
+    let name = document.getElementById('name');
+    let surname = document.getElementById('surname');
+    let email = document.getElementById('email');
+    let phone = document.getElementById('phone');
+    let text = document.getElementById('dato');
+    
+    userBooked = new user(name.value, surname.value, email.value, phone.value, text.value)
+    
+    
+    if ((((userBooked.name == '') || (userBooked.surname == '')) || (userBooked.email == '')) || (userBooked.phone == '')) {
+        //SE REEMPLAZA ALERT POR SWEAT
+         //alert('Sorry, but you must complete the form to submit!!!')
+         Swal.fire({
+            title: 'Sorry, but you must complete the form to be able to send it!!!',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+        userBooked += new user(name.value, surname.value, email.value, phone.value, text.value);
+    }else{
+        Swal.fire({
+            title: 'Great, your booked were processed correctly!!!',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+
+    } 
+    
+    localStorage.setItem('userBooked', JSON.stringify(userBooked))
+    console.log(userBooked);
+    console.log(reserva)
+}
+
+buttonSubmit = document.getElementById('buttonSubmit');
+
+buttonSubmit.onclick = (e) => {
+    e.preventDefault()
+    endBooked()
+    window.location.href = "#bookedInProgress";
+}
+
+//-----------------------check-in-out-------------------------------//
+
+//Cargo guests
+let guests = [ '', 1, 2];
+let pasajeros = document.getElementById('pasajeros'); 
+
+function opciones(arrayGuests, opcion){
+    let guestsChoose = "";
+    for (let i = 0; i <arrayGuests.length; i++){
+        guestsChoose += "<option>" + arrayGuests[i] + "</option>";
+    }
+    opcion.innerHTML = guestsChoose
+} 
+opciones(guests, pasajeros)
+
+//Guests select
+function obtenerPasajero(){
+    let pasajeroSelect = document.getElementById("pasajeros");
+    localStorage.setItem(pasajeroSelect.id, pasajeroSelect.value)
+}
+
+Date
+let actualDate;
+function fechaActualizada(){
+    actualDate = new Date();
+    //console.log(actualDate.getTime())
+}
+fechaActualizada()
+
+
+// funcion onbtener Date
+function obtenerFecha(){   
+    localStorage.setItem(checkIn.id, checkIn.value);
+    localStorage.setItem(checkOut.id, checkOut.value);
+   
+    checkInDate = new Date(checkIn.value);
+    checkOutDate = new Date(checkOut.value);
+    //console.log(checkInDate.getTime())
+
+    //Se reemplaza por TERNARIO
+    //SE reemplaza alert por SWEAT
+    checkInDate = (checkInDate.getTime() < actualDate.getTime()) ?
+        Swal.fire({ icon: 'warning', title: 'Mistake', text: "You must choose a valid date for your checkin!!!"
+                      + "\n Today is: " + actualDate, showConfirmButton: false, timer: 7000
+              })
+        : new Date(checkIn.value);
+        
+    //Se reemplaza por TERNARIO
+    //Se reemplaza alert por Sweat
+    checkOutDate = (checkOutDate <= checkInDate ) ? 
+        Swal.fire({ icon: 'warning', title: 'Mistake', text: "You must choose a date after the checkin date!!!", showConfirmButton: false, timer: 5000})
+        : new Date(checkOut.value);
+  
+    // //ver validacion
+    // if (localStorage.getItem(dateTotal) == NaN) {
+    //     alert("debe definir fechas")
+    // } else {
+    //     alert("Ha reservado esta habitacion por "+dateTotal/(1000 * 60 * 60 * 24)+" noche/s") 
+    // }
+}
+
+function pasajeroDatos(){
+    obtenerFecha()
+    obtenerPasajero()
+    
+    if(checkIn.value,checkOut.value,pasajeros.value === null || checkIn.value,checkOut.value,pasajeros.value === ''){
+        //alert("Select a date and guests to continue")
+        let timerInterval
+        Swal.fire({
+            title: 'Select a date and guests to continue!',
+            timer: 2500,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                //b.textContent = Swal.getTimerLeft()
+                }, 100)
+            },
+            willClose: () => {
+                clearInterval(timerInterval)
+            }
+        }).then((result) => {
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer')
+            }
+        })
+    }else{
+        let dateTotal = 0
+        dateTotal += checkOutDate - checkInDate;
+        localStorage.setItem("dateTotal", Math.floor(dateTotal / (1000 * 60 * 60 * 24)));
+        //SE REEEMPLAZA ALERT POR TOASTIFY
+        //alert("Ha reservado esta habitacion por "+dateTotal/(1000 * 60 * 60 * 24)+" noche/s")
+        Toastify({
+            text:"Ha reservado esta habitacion por "+ dateTotal/(1000 * 60 * 60 * 24)+" noche/s",
+            duration: 5000,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: " linear-gradient(to right, rgb(106, 6, 236), rgb(220, 0, 240)",
+            },
+            offset: {
+                x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                y: 100,// vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+        }).showToast();
+         // SE AGREGA SETTIMEOUT   
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000); 
+    }
+}
+
+// funcion para vaciar el storage si no hay reservas seleccionadas
+function refreshStorage () {
+        checkIn = '';
+        checkOut = '';
+        pasajeroSelect = '';
+        userBooked = {};
+        dateTotal = 0;
+        localStorage.setItem('dateTotal',JSON.stringify(dateTotal))
+        localStorage.setItem('check-in',JSON.stringify(checkIn));
+        localStorage.setItem('check-out',JSON.stringify(checkOut));
+        localStorage.setItem('dateTotal',JSON.stringify(dateTotal));
+        localStorage.setItem('pasajeros',JSON.stringify(pasajeroSelect));
+        localStorage.setItem('userBooked', JSON.stringify(userBooked));
+    }
---
+++ scripts/script.js
@@ -0,0 +1,202 @@
+
+//-----------------------------------------------------//
+//declaracion variables globales
+let reserva;
+
+//Nuestra reserva la guardamos en el localStorage
+// se reemplaza por TERNARIO
+reserva = (JSON.parse(localStorage.getItem('reserva'))) ? 
+    reserva = JSON.parse(localStorage.getItem('reserva')):
+    localStorage.setItem('reserva', JSON.stringify([]))
+    reserva = JSON.parse(localStorage.getItem('reserva'));
+
+
+//muestra las card de manera dinamica reemplazando al html
+function showRooms() {
+    //tbm podia usar un foreach
+    for (let i= 0; i< rooms.length; i++) {
+        const element = rooms[i];
+        //creo una variable para evitar escribir element
+        const {id, category, name, price, img} = element
+        const card =  `
+        <div class='card'>
+            <div class='box'>
+                <img src="${img}" alt="">    
+                <div class="head">${name}</div>
+                <div class="subT">${category}</div>
+                <div class="price">$${price.toLocaleString()}- Euros</div>
+                <button id=${id} class="btnAdd btn">Booked</button>
+            </div>
+        </div>
+        `
+        const contenedora = document.getElementById('contenedora')
+        contenedora.innerHTML += card
+    } 
+}
+
+//llamo a la function
+showRooms();
+
+//Agrego habitacion por medio de evento
+const btnAdd = document.getElementsByClassName('btnAdd')
+for (let i = 0; i < btnAdd.length; i++) {
+    const element = btnAdd[i];
+    element.addEventListener('click', addReserve)
+}
+
+//funcion para asociar el click con la habitacion por medio del ID
+function addReserve(e){
+    //capturamos el evento en una constante por id
+    const btn = e.target;
+    const idBtn = btn.getAttribute('id')
+    const roomSelect = rooms.find(prod => prod.id == idBtn)
+    //acumula los click del mismo prod
+    const inReserve = reserva.find(prod => prod.id == roomSelect.id)
+    //Condicional que acumula reservas iguales para que no se repitan
+    if (!inReserve) {
+        //si no esta lo pusheo
+        reserva.push({...roomSelect, cantidad: 1})
+        //alert("Great Opcion")
+        Swal.fire({
+            position: 'top-right',
+            icon: 'success',
+            title: 'Great Opcion!!!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }else{
+        //si esta lo borro y
+        let filterReserve = reserva.filter(prod => prod.id != inReserve.id)
+        //reescribo la cantidad agregandole 1
+        reserva = [...filterReserve, {...inReserve, cantidad: inReserve.cantidad + 1}]
+    }
+    console.log(reserva)
+    localStorage.setItem('reserva', JSON.stringify(reserva))
+    setTimeout(() => {
+        window.location.reload();
+    }, 1000); 
+}
+
+//Acumulador de seleccion se actualiza cada vez que refresh
+const contador = document.getElementById('counter')
+contador.innerHTML = reserva.length 
+
+//-----------------------------------------------------//
+
+//Filtro or categoria
+
+let searcher = document.getElementById('inputSearch');
+let btnSearcher = document.getElementById('filtrar');
+
+function filterByCategory() {
+    //creo elemento bottom
+    let showAllRooms = document.createElement('button')
+    showAllRooms.setAttribute("class","btn")
+    showAllRooms.innerHTML = ('showAllRooms')
+    //filtro lista por categoria
+    const filteredProduct = rooms.filter((room) => room.category === searcher.value)
+    console.log(filteredProduct)
+    //replico html por nueva lista filtrada
+    filteredProduct.forEach((element) => {
+            const card =  `
+            <div class='card'>
+                <div class='box'>
+                    <img src="${element.img}" alt="">    
+                    <div class="head">${element.name}</div>
+                    <div class="subT">${element.category}</div>
+                    <div class="price">$${element.price.toLocaleString()}- Euros</div>
+                    <button id=${element.id} class="btnAdd btn">Booked</button>
+                </div>
+            </div>
+            `
+        const contenedora = document.getElementById('contenedora')
+        contenedora.innerHTML += card
+    });
+    showAllRooms.onclick = () =>{
+        contenedora.innerHTML = ''
+        showRooms();
+    }
+
+    //llamo al btn filtrar
+    buttonFilter.append(showAllRooms)
+
+    //Llamo a la function addReserva mediante el onclick ya declarado
+    const btnAdd = document.getElementsByClassName('btnAdd')
+    for (let i = 0; i < btnAdd.length; i++) {
+        const element = btnAdd[i];
+        element.addEventListener('click', addReserve)
+    }
+}
+
+//Eventos del filtro por categoria
+searcher.onchange = () =>{
+    contenedora.innerHTML = ''
+    const buttonFilter = document.getElementById('buttonFilter')
+    buttonFilter.innerHTML = '';    
+    filterByCategory()
+}
+btnSearcher.onclick = () =>{
+    contenedora.innerHTML = '';
+    const buttonFilter = document.getElementById('buttonFilter')
+    buttonFilter.innerHTML = '';
+    filterByCategory()
+}
+
+//---incluimos de manera dinamica html desde API simulada por json a traves de fetch----//
+
+//1RO fetch sobre data
+const fetchLocalData = () =>{
+    fetch('scripts/data.json').then((response) => response.json())
+        .then((result) => {
+            renderAboutUs(result)
+           // console.log(result)
+        }).catch((err)=>{
+            console.log(err)   
+        })
+}
+fetchLocalData()
+    
+//2do render sobre api data.json
+// 2 agrego el codigo
+const renderAboutUs = (body) => {
+    //console.log(body)
+    const bodyAboutUs =  `
+        <h1 class="title">${body.mainTittle}</h1>
+        <div class="carousel">
+            <div class='subTbox'>
+                <h2 class="subT subTA">${body.subTittle.text1}</h2>
+                <h2 class="subT subTB">${body.subTittle.text2}</h2>
+                <h2 class="subT subTC">${body.subTittle.text3}</h2>
+            </div>
+            <div class='contCardBox'>
+                <div class='cardBox'>
+                    <div class='box'>
+                        <i class="${body.cardA.icon1}"></i>
+                        <div class="text">${body.cardA.tMission}</div>
+                        <p class="pText">${body.cardA.mission}</p>
+                    </div>
+                </div>
+                <div class='cardBox'>
+                    <div class='box'>
+                        <i class="${body.cardB.icon2}"></i>
+                        <div class="text">${body.cardB.tValues}</div>
+                        <p class="pText">${body.cardB.values}</p>
+                    </div>
+                </div>
+                <div class='cardBox'>
+                    <div class='box'>
+                        <i class="${body.cardC.icon3}"></i>
+                        <div class="text">${body.cardC.tTarget}</div>
+                        <p class="pText">${body.cardC.target}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+            `
+    let contAboutUs = document.getElementById('contAboutUs')
+    contAboutUs.innerHTML += bodyAboutUs
+}
+
+
+
+
